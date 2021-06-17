61cc4bb12d68ed0791f0f1797ec152f53ddeb52c
Jason117 <bboyace10@gmail.com>
2021-06-17 08:47:03 -0500

    CoverPage

---
+++ src/components/Body.js
@@ -0,0 +1,7 @@
+export const Body = () => {
+    return (
+        <div class="w-full h-screen bg-gray-500">
+            
+        </div>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+// tailwind.config.js
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage: theme => ({
+      'header-image': "url('/src/Img/background.jpg')",
+     })},
+     colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+      brown: {
+        brown: '#433333'
+       },
+     }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
